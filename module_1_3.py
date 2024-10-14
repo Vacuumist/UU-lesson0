d81f0cf82e256f337b59a7e128a4a4cbf6eab9c3
Anatoly Ivanov <beeranatoly@gmail.com>
2024-10-14 20:34:32 +0300

    Update module_1_3.py
    Added spaces before comments

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,8 @@
-name = 'Anatoly'   # Creates a variable "name" with a specified string value.
-print('Name:', name)  # Outputs a string 'Name:' and the value of the variable "name".
-age = 42  # Creates a variable "age" with specified integer value.
-print('Age:', age)  # Outputs a string 'Age:' and the value of the variable "age".
-age = age + 1  # Rewrites a value of a variable "age" with a new integer value increased by 1.
-print('New Age:', age)  # Outputs a string 'New Age:' and the updated value of the variable "age".
-is_student = True  # Creates a variable "is_student" with a specified boolean value.
+name = 'Anatoly'                  # Creates a variable "name" with a specified string value.
+print('Name:', name)              # Outputs a string 'Name:' and the value of the variable "name".
+age = 42                          # Creates a variable "age" with specified integer value.
+print('Age:', age)                # Outputs a string 'Age:' and the value of the variable "age".
+age = age + 1                     # Rewrites a value of a variable "age" with a new integer value increased by 1.
+print('New Age:', age)            # Outputs a string 'New Age:' and the updated value of the variable "age".
+is_student = True                 # Creates a variable "is_student" with a specified boolean value.
 print('Is Student:', is_student)  # Outputs a string 'Name:' and the value of the variable "name".
