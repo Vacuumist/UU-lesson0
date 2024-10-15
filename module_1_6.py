ae22d8a692812f2ffaf5263c449be8db6f0959a4
Anatoly Ivanov <beeranatoly@gmail.com>
2024-10-15 20:48:07 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,7 @@
+# Homework for Module 1, topic "Variables".
+homework_completed = 12
+homework_time_spent_total = 1.5
+course_name = "Python"
+homework_time_average = homework_time_spent_total / homework_completed
+print("Курс:", course_name + ", всего задач:", str(homework_completed) + ", затрачено часов:",
+      str(homework_time_spent_total) + ", среднее время выполнения:", homework_time_average, "часа.")
