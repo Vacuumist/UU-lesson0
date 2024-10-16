f879f638f32e98c55a27a08caf8229b34084a7c6
Anatoly Ivanov <beeranatoly@gmail.com>
2024-10-16 06:45:12 +0300

    Add files via upload

---
+++ module_1_9.py
@@ -0,0 +1,7 @@
+# Homework for Module 1, topic "Strings and String Indexing".
+example = "This is an example string"
+print(example[0])       # Outputs the first character
+print(example[-1])      # Outputs the last character
+print(example[12:])     # Outputs the 2nd half
+print(example[::-1])    # Outputs the string in reverse order
+print(example[1::2])    # Outputs every second character
