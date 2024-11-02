c5b80d7f99afba15cfe1a4f06bfb36fbfaf91d0b
Vacuumist <beeranatoly@gmail.com>
2024-11-02 23:21:46 +0300

    Added file

---
+++ module_3_2.py
@@ -0,0 +1,27 @@
+def not_email(address):     # Проверка на формат e-mail и домены.
+    wrong_address = False
+    if '@' not in address:
+        wrong_address = True
+    if ".com" not in address and ".ru" not in address and ".net" not in address:
+        wrong_address = True
+    return wrong_address
+
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    recipient = recipient.lower()
+    sender = sender.lower()
+    if not_email(recipient) or not_email(sender):
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', str(recipient) + '.')
+    elif sender == recipient:
+        print('Невозможно отправить письмо самому себе!')
+    elif sender == "university.help@gmail.com":
+        print('Письмо успешно отправлено с адреса', sender, 'на адрес', str(recipient) + '.')
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', str(recipient) + '.')
+
+# Tests
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender = 'urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email(sender='University.HELP@gmail.com', recipient='turban.student@mail.ru', message='Доп. тест 1')
+send_email('Доп. тест 2', 'University.HELP@gmail.com')
