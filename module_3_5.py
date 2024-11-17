ed1cb6ee4e69766d04e54a9457460515730c59be
Vacuumist <beeranatoly@gmail.com>
2024-11-17 18:12:56 +0300

    Edited module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,12 +1,14 @@
 # Recursive multiplication
 def get_multiplied_digits(number):
+    while number % 10 == 0 and number != 0:
+        number = int(number / 10)
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    elif number == 0: # защита от умножения на ноль, стоящий последним.
-        return 1
     else:
         return first
 
-print(get_multiplied_digits(40203))
+print(get_multiplied_digits(40203))     # = 24
+print(get_multiplied_digits(4020300))   # = 24
+print(get_multiplied_digits(0))         # = 0
