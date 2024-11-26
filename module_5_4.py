2184db606b479bb6dd5b5184d1a1a03e86d3943e
Vacuumist <beeranatoly@gmail.com>
2024-11-26 21:17:32 +0300

    module_5_4.py added

--- module_5_4.py
+++ module_5_4.py
@@ -2,7 +2,7 @@ class House:
 
     houses_history = []
     def __new__(cls, *args, **kwargs):
-        cls.houses_history += [args[0]]
+        cls.houses_history.append(args[0])
         return super().__new__(cls)
 
     def __init__(self, name, number_of_floors):
