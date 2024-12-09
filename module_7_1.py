b0f00fed6afb0003f0b9e84c08017c2686dd3239
Vacuumist <beeranatoly@gmail.com>
2024-12-09 22:35:33 +0300

    Added module_7_1.py

---
+++ module_7_1.py
@@ -0,0 +1,39 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        info = file.read()
+        file.close()
+        return info
+
+    def add(self, *products):
+        warehouse = self.get_products()
+        for p in products:
+            if str(p) not in warehouse:
+                file = open(self.__file_name, 'a')
+                file.write(f'{str(p)}\n')
+                file.close()
+            else:
+                print(f'Продукт {p.name} уже есть в магазине')
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
